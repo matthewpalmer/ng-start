1215c20f3025022c4fced1bd3d363248dafd128f
Matthew Palmer <matt@palmer.im>
2013-06-23 14:18:53 +1000

    simple test eg

---
+++ test/controllersSpec.js
@@ -0,0 +1,8 @@
+describe('controllers', function() {
+  beforeEach(module('employeesList.controllers'));
+  it('should do something', inject(function() {
+    //do something
+    expect(1).to.be(ok);
+  }));
+});
+    
